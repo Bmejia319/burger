f29ca79cee78ec663adcf8ceccfcc97878559d8c
Brian Mejia <Bmejia319@gmail.com>
2018-09-22 18:19:42 -0400

    new changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,10 @@ var express = require("express");
 var router = express.Router();
 
 var burgers = require("../models/burger.js"); 
+
+
+router.get("/", function(req, res) {
+	res.render("index");
+})
+
+module.exports = router;
