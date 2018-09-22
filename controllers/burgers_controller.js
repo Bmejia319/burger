aa43698432a9c74b84eea756321037a1ffdc1236
Brian Mejia <Bmejia319@gmail.com>
2018-09-22 19:28:31 -0400

    update to controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,11 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 
-var burgers = require("../models/burger.js"); 
-
+var burger = require("../models/burger.js"); 
 
 router.get("/", function(req, res) {
-	res.render("index");
+	burger.all(function(burger_data) {
+		console.log(burger_db);
+		res.render("index");
+	})
 })
 
 module.exports = router;
