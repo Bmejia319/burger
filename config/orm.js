de035b60268004cd8d3d5a19c839769f536757a7
Brian Mejia <Bmejia319@gmail.com>
2018-09-23 01:32:14 -0400

    changes

--- config/orm.js
+++ config/orm.js
@@ -39,3 +39,67 @@ function objToSql(ob) {
 }
 
 // Object for all SQL statements
+var orm = {
+	all: function(tableInput, cb) {
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		connection.query(queryString, function(err, result) {
+			if(err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+
+	create: function(table, cols, vals, cb) {
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		console.log("queryString");
+
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+
+	update: function(table, objColVals, condition, cb) {
+		var queryString = "Update " + table;
+
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+		
+		console.log(queryString);
+		connection.query(queryString, function(err, result) {
+			if(err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+
+	delete: function(table, condition, cb) {
+		var queryString = "DELETE FROM " + table;
+		queryString += " WHERE ";
+		queryString += condition;
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	}
+
+};
+
+module.exports = orm
